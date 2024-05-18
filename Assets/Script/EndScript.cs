01adf5af3da99157469a2840b73cbb3af15583cb
SamLee <sam920507@gmail.com>
2024-05-19 06:01:35 +0800

    EndScene

--- Assets/Script/EndScript.cs
+++ Assets/Script/EndScript.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndScript : MonoBehaviour
 {
     [SerializeField] public string[] dialogue;
+    [SerializeField] public Sprite[] CgImages;
 
+    [SerializeField] private Image player;
+    [SerializeField] private Image CG;
+    [SerializeField] TextMeshPro dia;
+
+    [SerializeField] private int maxdia;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+
+        }
     }
 }
